63356b8efcae065c02eaf28158d4c26daa8a6f7d
fpc <FrancisChmiel@hotmial.co.uk>
2019-07-02 15:58:43 +0100

    Make tests consistent with sklearn estimator object

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-import datascipy as dsp
+import pycompete as compete
 
 # To do:
 # make the Ensembler test case consistent with sklearn objects.
@@ -11,7 +11,7 @@ class EnsemblerTestCase(unittest.TestCase):
     def setUp(self):
         """Creates example targets and predictions array."""
 
-        global targets, predictions, true_averages, example_weights
+        global targets, predictions, true_averages, ex_weights
         global weighted_average_predictions
 
         targets = np.array([0,1,1,0,1,1,0,1])
@@ -28,8 +28,8 @@ class EnsemblerTestCase(unittest.TestCase):
         true_averages = np.array([1,1,1,2,2,2,0,3])/3
         
         # weights of each classifier and weighted predictions
-        example_weights= np.array([0.5, 0.2, 0.5])
-        weighted_average_predictions = (example_weights*predictions).sum(axis=1)
+        ex_weights= np.array([0.5, 0.2, 0.5])
+        weighted_average_predictions = (ex_weights*predictions).sum(axis=1)
     
     def tearDown(self):
         """TearDown"""
@@ -37,31 +37,39 @@ class EnsemblerTestCase(unittest.TestCase):
     
     def test_preserves_number_of_rows(self):
         """Verifys that averaging is performed along the columnar axis."""
-        ensemble_predictions = dsp.AveragingEnsemble(predictions)
-        self.assertTrue(ensemble_predictions.shape[0]==predictions.shape[0])
-        self.assertTrue(ensemble_predictions.shape[1]==1)
+        ensembler = compete.Ensembler(method='mean')
+        ensemble_ps = ensembler.fit_transform(predictions, targets)
+        self.assertTrue(ensemble_ps.shape[0]==predictions.shape[0])
+        self.assertTrue(ensemble_ps.shape[1]==1)
     
     def test_rows_average_correctly(self):
-        """Verifys that the sum of each row is correct"""
-        ensemble_predictions = dsp.AveragingEnsemble(predictions)
-        self.assertTrue(all(ensemble_predictions==true_averages))
+        """Verifys that the sum of each row is correct when the mean
+        method is used."""
+        ensembler = compete.Ensembler(method='mean')
+        ensemble_ps = ensembler.fit_transform(predictions, targets)
+        self.assertTrue(all(ensemble_ps==true_averages))
 
     def test_weights_are_applied(self):
-        """Verify that when weights are applied they are added correctly."""
-        ensemble_predictions = dsp.AveragingEnsemble(predictions,
-                                                     weights=example_weights)
-        self.assertTrue(all(ensemble_predictions==weighted_average_predictions))
+        """Verify correct ensembled predictions when weights applied."""
+        ensembler = compete.Ensembler(method='weighted', weights=ex_weights)
+        ensemble_ps = ensembler.fit_transform(predictions, targets)
+        self.assertTrue(all(ensemble_ps==weighted_average_predictions))
 
     def test_only_accept_numpy_arrays(self):
         """Verifys TypeError exception is raised when a numpy array is not
         provided"""
-
         with self.assertRaises(TypeError):
-            dsp.ensemble_predictions(1)
+            ensembler = compete.Ensembler()
+            ensembler.fit_transform(1, targets)
         with self.assertRaises(TypeError):
-            dsp.ensemble_predictions(predictions, targets=1)
+            ensembler = compete.Ensembler()
+            ensembler.fit_transform(predictions, 1)
         with self.assertRaises(TypeError):
-            dsp.ensemble_predictions(1, targets=1)
+            ensembler = compete.Ensembler()
+            ensembler.fit_transform('example', targets)
+        with self.assertRaises(TypeError):
+            ensembler = compete.Ensembler()
+            ensembler.fit_transform(predictions, 'example')
 
 if __name__ == '__main__':
     unittest.main()
