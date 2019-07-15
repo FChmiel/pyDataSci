2c2f8a55461cbbe00ffadfe165fd49166c3fbe4e
fpc <FrancisChmiel@hotmial.co.uk>
2019-07-15 11:23:15 +0100

    Add 'optimize' functionality to fit method
    - Add example usage to docstring
    - Add optimize using random weights

--- pycompete.py
+++ pycompete.py
@@ -1,15 +1,17 @@
 import warnings
 import numpy as np
+import itertools
 
 
 #relevant sklearn imports
 from sklearn.metrics import roc_auc_score
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.model_selection import StratifiedKFold
 
-
-class Ensembler(BaseEstimator, ClassifierMixin):
-    """Creates an (weighted) averaging ensemble of predictions.
+class BinaryEnsembler(BaseEstimator, ClassifierMixin):
+    """Creates an (weighted) averaging ensemble of predictions for
+    a binary classification problem.
       
     Parameters:
     -----------    
@@ -35,17 +37,20 @@ class Ensembler(BaseEstimator, ClassifierMixin):
 
     Examples:
     ---------
-    >>> from sklearn.datasets import load_iris
-    >>> from pycompete import Ensembler
-    >>> from sklearn.linearModel import LogisticRegression
+    >>> from sklearn.datasets import load_breast_cancer
+    >>> from sklearn.linear_model import LogisticRegression
     >>> from sklearn.svm import SVC
-    >>> X, y = load_iris(return_X_y=True)
-    >>> linear_clf = LogisticRegressionCV(cv=5, random_state=0).fit(X, y)
-    >>> svm_clf = SVC(probability=True).fit(X, y)
-    >>> ensembler = Ensembler(method='mean')    
+    >>> X, y = load_breast_cancer(return_X_y=True)
+    >>> linear_clf = LogisticRegression(solver='lbfgs').fit(X, y)
+    >>> linear_ps = linear_clf.predict_proba(X)[:,1]
+    >>> svm_clf = SVC(probability=True, gamma='auto').fit(X, y)
+    >>> svm_ps = svm_clf.predict_proba(X)[:,1]
+    >>> ensembler = Ensembler(method='mean')
+    >>> P = np.c_[linear_ps, svm_ps]
+    >>> ensembler.fit(P, y)
+    >>> ensembler_predictions = ensembler.predict(P)
+
     TO DO:
-        - Update to work for generic number of classes
-            - (do this by using dstack)
         - Add optimizing weights code.
     """
     _allowed_methods = ["mean", "weighted", "optimize"]
@@ -57,16 +62,17 @@ class Ensembler(BaseEstimator, ClassifierMixin):
     def _reset(self):
         """Reset prediction dependent state of the scaler."""
         self.weights_ = None
+        self.cv_scores_ = None
 
-    def fit(self, P, y, weights=None, verbose=False):
+    def fit(self, P, y, weights=None, verbose=False, init_rounds=10):
         """
         Generates the weights to average the ensemble.
 
         Parameters:
         -----------
         P : {array-like, sparse matrix}, shape [n_samples, n_classifers]
-            The independent predictions of each classifer to be used in the
-            ensemble.
+            The independent predictions of the positive class of each classifer
+            to be used in the ensemble.
 
         y : array-like, shape [n_samples]
             The target class of each sample in P.
@@ -76,6 +82,10 @@ class Ensembler(BaseEstimator, ClassifierMixin):
 
         verbose : bool, (default=False)
             Whether to print the classifier performance to screen.
+
+        init_rounds: int, (default=10)
+            Number of times to train ensemble with randomly initalized weights.
+            Used only if method="optimize".
         
         Returns:
         --------
@@ -93,12 +103,26 @@ class Ensembler(BaseEstimator, ClassifierMixin):
         self.weights_ = weights
 
         if self.method=='optimize':
-            msg = ("Optimization of weights is not yet implemented.")
-            raise Exception(msg)
-            # optimize weights if required. TODO
-
-        # calculate the cv scores of the ensemble
-    
+            ensemble_score = 0 # higher score is better
+            # check how individual model performs
+            for i in range(P.shape[1]):
+                ws = np.zeros(P.shape[1])
+                ws[i] = 1
+                ps = (P*ws).mean(axis=1)
+                score = self.metric(y, ps)
+                print('Model {0} score: {1:.3f}'.format(i, score))
+                if score>ensemble_score:
+                    self.weights_ = ws
+                    ensemble_score = score
+            # random weights
+            for _ in range(init_rounds):
+                ws = np.random.rand(P.shape[1])
+                ws = ws / np.sum(ws)
+                ps = (P*ws).mean(axis=1)
+                score = self.metric(y, ps)
+                if score>ensemble_score:
+                    self.weights_ = ws
+                    ensemble_score = score
         return self
     
     def predict(self, P, y=None):
