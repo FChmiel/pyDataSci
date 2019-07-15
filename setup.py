59fafed6b989d67a753b0d336994356d9041b0cd
fpc <FrancisChmiel@hotmial.co.uk>
2019-07-15 11:58:54 +0100

    Add files to help with packaging
    - Add setup.py
    - Add __init__.py files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pyCompete',
+    version='0.1dev',
+    packages=['pycompete',],
+    license='BSD 3-Clause "New" or "Revised" License',
+    long_description=open('README.txt').read(),
+)
