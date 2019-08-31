d3619f7ca8493fcd3de5e921e87b4c805e510481
FChmiel <Francis@Franciss-MacBook-Pro-2.local>
2019-08-31 21:58:35 +0100

    Add encoding.py file

---
+++ pycompete/encoding.py
@@ -0,0 +1,30 @@
+"""
+Encoding.
+"""
+
+
+
+# ideas: 
+# WOE encoder
+
+
+class FrequencyEncoder():
+
+	def __init__(self):
+		pass
+
+
+class TargetEncoder():
+	"""
+	Implementation of target encoding as demonstated in XXX.
+	"""
+
+	def __init__(self):
+		pass
+
+	def fit(X, y):
+		pass
+
+	def transform():
+		pass
+
