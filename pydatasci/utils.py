9d16c0f06db9013831d076a4ddbd5787df70573a
FChmiel <francispeterchmiel@gmail.com>
2020-02-16 14:29:04 +0000

    Add remove_axes function to utils.py

--- pydatasci/utils.py
+++ pydatasci/utils.py
@@ -1,1 +1,20 @@
-"""Function to help with plotting"""
+"""Utility functions used throughout pyDataSci.
+
+F. P. Chmiel
+"""
+
+# Plotting utility functions
+
+def remove_axis(ax):
+	"""
+	Removes the top and right axis from a matplotlib.Axes object.
+
+	Parameters:
+	-----------
+	ax, matplotlib.Axes
+		Axis to remove the top and right axis from.
+	"""
+	for loc in ['right', 'top']:
+		ax.spines[loc].set_visible(False)
+	for loc in ['left', 'bottom']
+		ax.yaxis.set_ticks_position(loc)
