5703b20497be7ab0f33d620f111eacf92f2e48d1
fpc <fpc@it-innovation.soton.ac.uk>
2020-04-16 17:28:11 +0100

    Finish save_record function

--- pydatasci/modelling/logging.py
+++ pydatasci/modelling/logging.py
@@ -1,9 +1,10 @@
 """
 Sets of functions for helping to log the results of experiments
 """
+import os
+import json
 
-
-def save_record(run_name, summary, seed=None, fpath=None, number='auto'):
+def save_record(run_name, summary, fdir='results/', number='auto'):
     """
     Saves provided summary and results of a model as a json file.
 
@@ -15,18 +16,37 @@ def save_record(run_name, summary, seed=None, fpath=None, number='auto'):
     summary : dict, 
         Dictionary summarizing the model and any associated results.
 
-    seed : int (default=None),
-        The random seed used in the model training.
-
-    fpath : str (default=None),
-        Path to save the result to. If None, a new folder is created named
-        'results' in the current directory.
+    fdir : str (default='results/'),
+        Path to save the result to. If not provided, a new folder is created
+        named 'results' in the current directory.
 
     number : {int, str} (default='auto'),
         Numerical suffix to the filename. If auto the function checks the 
         specified directory for files of the same name and increments the
         numerical suffix by one if the file exists.
     """
+    
+    # create directory if not already existing
+    if not os.isdir(fdir):
+        try:
+            os.mkdir(fdir)
+        except OSError:
+            print(f'Creation of {fdir} failed.')
+    
+    # create file name
+    if number=='auto':
+        number = 0
+        fname = fdir + run_name + f'_{number}'
+        while os.path.isfile(fname):
+            number += 1
+            fname = fdir + run_name + f'_{number}'
+    else:
+        fname = fdir + run_name + f'_{number}'
+
+    # save the file
+    json.dump(summary, open( f'{fname}.json', 'w' ) )
+   
+
 
 def create_run_summary():
     """
